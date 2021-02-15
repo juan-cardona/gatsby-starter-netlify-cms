f460fd66e178de73102c39e6eff5097b44e7804c
grupodog <grupodog@gmail.com>
2021-02-15 15:14:58 -0600

    modifies gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
@@ -64,12 +65,6 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-      {
-        resolve: 'gatsby-plugin-sass',
-        options: {
-          indentedSyntax: true
-        }
-      },
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
